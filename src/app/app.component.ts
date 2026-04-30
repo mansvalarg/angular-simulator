4e29ca4188c32834a816d06d1c096323417f6925
mansvalarg <mans.valarg@gmail.com>
2026-04-30 20:54:42 +0300

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Color } from '../enums/Color.js';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-
 export class AppComponent {
+
   companyName: string = 'РУМТИБЕТ';
 
   constructor() {
